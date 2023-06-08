18eb37ee075fc6e0fea28535dbedbd2f0f66c6bb
“Ymishok321 <kkoiku3@gmail.com>
2023-06-08 13:42:15 +0300

    Removed redunant title

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import QRCode from 'react-qr-code';
 
 function App() {
   const inputClasses = `border border-gray-300 w-full h-8 p-4 font-light text-sm focus:outline-none
-    focus:border-gray-600`;
+    focus:border-gray-600 mt-10`;
 
   const [inputValue, setInputValue] = React.useState('');
 
   return (
     <div className='container mx-auto w-2/4'>
-      <h1 className='py-4'>
-        Введите ссылку для генерации QR-кода:
-      </h1>
       <input
         onChange={e => setInputValue(e.target.value)}
         placeholder='https://www.youtube.com/watch?v=0MRJBbylyzs'
