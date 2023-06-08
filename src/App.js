c08054d76a1a7acecf4e4fcefab9cc8dddb6c697
“Ymishok321 <kkoiku3@gmail.com>
2023-06-08 13:39:31 +0300

    Removed redunant files & comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import './App.css';
 import QRCode from 'react-qr-code';
 
 function App() {
   const inputClasses = `border border-gray-300 w-full h-8 p-4 font-light text-sm focus:outline-none
-    focus:border-gray-600`; // перенос из-за того что строка слишком длинная и занимает много места
-    // в редакторе
+    focus:border-gray-600`;
 
   const [inputValue, setInputValue] = React.useState('');
 
